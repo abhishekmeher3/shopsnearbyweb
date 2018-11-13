1dcb3a96a6d9d4cb111e474edfc64b34f0472371
Abhishek Meher <devaskbiz@gmail.com>
2018-11-14 00:25:21 +0530

    signup component added.

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../../core/services/user.service';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  names = ["User", "Shop Owner", "Agent"]
+  email:string
+  phonenum:number
+  userType = this.names[0]
+
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
   }
 
+  signup(){
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position)
+    });
+  }
+  signup2() {
+    this.userService.login("sainik18@gmailll.com", "ZrLnMk").subscribe(user=>{
+      this.email = user.email
+      this.phonenum = Number(user.phoneNumber)
+    },error=>{      
+      this.email = error.error.message
+    })
+  }
 }
