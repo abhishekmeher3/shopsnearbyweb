b2f5a27f41822bef645ba7f5864269c29f4553be
Abhishek Meher <devaskbiz@gmail.com>
2018-11-15 23:50:22 +0530

    signup component done. Added material dialog. Added material progressbar. Added material Button.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,31 @@ import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { loginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule, MatButtonModule, MatProgressBarModule} from '@angular/material';
+import { InfoDialogComponent } from './info-dialog/info-dialog.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
     loginComponent,
+    InfoDialogComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatProgressBarModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    InfoDialogComponent
+  ]
 })
 export class AppModule { }
--- src/core/services/user.service.ts
+++ src/core/services/user.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { User } from '../models/user.model';
+import { } from 'googlemaps';
+import { LatLng, ResolvedAddress } from '../models/Models';
 
 @Injectable({
   providedIn: 'root'
@@ -20,10 +22,42 @@ export class UserService {
     return this.http.post<User>("http://122.175.60.20:8104/users/login", body.toString(), options)
   }
 
-  signup(email:string, phone:string, userType:string): Observable<object>{
+  signup(user: User): Observable<any>{
     let body = new FormData();
-    body.append('email', email);
-    body.append('phoneNumber', phone);
-    return this.http.post<User>("http://122.175.60.20:8104/users/login", body.toString())
+    body.append('firstName', user.firstName)
+    body.append('lastName', user.lastName)
+    body.append('email', user.email)
+    body.append('phoneNumber', user.phoneNumber)
+    body.append('address', user.address)
+    body.append('latitude', user.latitude.toString())
+    body.append('longitude', user.longitude.toString())
+    body.append('type', user.type)
+    return this.http.post<any>("http://122.175.60.20:8104/users/signup", body)
   }
+
+  resolveAddress(latlng: LatLng): Observable<ResolvedAddress>{
+    return Observable.create(observer=>{
+      let geocoder = new google.maps.Geocoder();
+      geocoder.geocode({
+        location: {
+          lat: latlng.latitude,
+          lng: latlng.longitude
+        }
+      }, (results, status) => {
+        if (status == google.maps.GeocoderStatus.OK) {
+          let resolvedAddress: ResolvedAddress = {
+            latlng : latlng,
+            formattedAddress: results[0].formatted_address
+          }
+          observer.next(resolvedAddress)
+          observer.complete()
+        }else{
+          observer.error({message: "Geocoder Failed"})
+          observer.complete()
+        }
+      })
+    })
+    
+  }
+
 }
