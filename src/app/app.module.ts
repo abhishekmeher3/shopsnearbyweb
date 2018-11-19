f4395adea0a5a91d5fd2d04418db522b145d4208
abhishek1993 <abhishekkhatua19@gmail.com>
2018-11-19 23:59:16 +0530

    created add shop component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,24 +7,18 @@ import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { loginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
-<<<<<<< HEAD
 import { AddShop } from './AddShop/add-shop.component';
-=======
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule, MatButtonModule, MatProgressBarModule} from '@angular/material';
 import { InfoDialogComponent } from './info-dialog/info-dialog.component'
->>>>>>> b2f5a27f41822bef645ba7f5864269c29f4553be
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
     loginComponent,
-<<<<<<< HEAD
     AddShop,
-=======
     InfoDialogComponent,
->>>>>>> b2f5a27f41822bef645ba7f5864269c29f4553be
   ],
   imports: [
     BrowserModule,
