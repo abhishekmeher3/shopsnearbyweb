13e903d29732840b62ff4592880168d218ae04db
Abhishek Meher <devaskbiz@gmail.com>
2018-12-26 18:29:22 +0530

    filters page 1st implementation.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import {Routes, RouterModule} from '@angular/router';
 import {SignupComponent} from './signup/signup.component';
 import {loginComponent} from './login/login.component';
 import { AddShop } from './AddShop/add-shop.component';
+import { FiltersPageComponent } from './filters-page/filters-page.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: 'login', component: loginComponent},
   {path: 'signup', component: SignupComponent},
-  {path:'addShop',component:AddShop}
+  {path:'addShop',component:AddShop},
+  {path: 'filters', component: FiltersPageComponent}
 ];
 
 @NgModule({
