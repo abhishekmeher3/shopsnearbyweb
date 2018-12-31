aa626421e7508532033bbf74d378b0673f3a22f5
Sushree Ranjan Moharana <sushreeranjan.moharana@factset.com>
2018-12-31 17:17:50 +0530

    Bootstrap and font-awesome
    1. Added bootstrap and font-awesome packages to dev dependencies
    2. Added bootstrap and font-awesome css's into app.component.css
    3. Removed bootstrap.css from index.html
    4. Added initial template for home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -9,6 +9,8 @@ export class HomePageComponent implements OnInit {
   test: string;
   currentLocation: string = 'Banjara hills';
   loading: boolean = false;
+  trendingSearches: String[] = ['Gyms, Car Repair, Medical Hall'];
+  // popularCategories =
 
   ngOnInit() {
     this.test = 'Home page..';
