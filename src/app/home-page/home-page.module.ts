f29d7c9b8076a0bc35f0fc2a2a541eb15f9cad95
Sushree Ranjan Moharana <sushreeranjan.moharana@factset.com>
2019-01-04 03:12:56 +0530

    created home page

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -1,14 +1,22 @@
 import {HomePageComponent} from './home-page.component';
 import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {
   MatDialogModule,
   MatButtonModule,
   MatProgressBarModule,
 } from '@angular/material';
+import {SlickModule} from 'ngx-slick';
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [MatDialogModule, MatButtonModule, MatProgressBarModule],
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    CommonModule,
+    SlickModule,
+  ],
   providers: [],
   bootstrap: [HomePageComponent],
 })
