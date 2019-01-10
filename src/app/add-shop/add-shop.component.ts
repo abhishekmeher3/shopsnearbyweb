447fbee4f6acb4df8254099c43fe41f67f94be17
Sushree Ranjan Moharana <sushreeranjan.moharana@factset.com>
2019-01-10 12:31:31 +0530

    Did following:
    1. Added validation for add-shop form
    2. Added styles to add-shop form

--- src/app/add-shop/add-shop.component.ts
+++ src/app/add-shop/add-shop.component.ts
@@ -3,22 +3,20 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-AddShop',
   templateUrl: './add-shop.component.html',
-  styleUrls: ['./add-shop.component.css'],
+  styleUrls: ['./add-shop.component.scss'],
 })
 export class AddShop implements OnInit {
-  categories = [
-    {
-      name: 'Toys',
-    },
-    {
-      name: 'Automobile',
-    },
-    {
-      name: 'Electricity',
-    },
-  ];
-  selectedValue = this.categories[0];
+  categories = ['Toys', 'Automobile', 'Electricity'];
+  timeComparisonFlag = false;
   constructor() {}
 
   ngOnInit() {}
+  onSubmit(form) {
+    console.log(form);
+    console.log('Successfully submitted the form.');
+    //Post the form and reset it by calling form.resetForm();
+  }
+  compareTime(startTime, endTime) {
+    return startTime < endTime;
+  }
 }
