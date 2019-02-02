95dd2550cf07f1539034b0d8f6551a6a6b15189c
Abhishek Meher <devaskbiz@gmail.com>
2019-02-02 22:02:43 +0530

    Enhancement: Login now shows error dialogs for user. Validations done. Better loading bar.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {LoginService} from './login.service';
-import {UserService} from '../../core/services/user.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from './login.service';
+import { UserService } from '../../core/services/user.service';
+import { MatDialog } from '@angular/material';
+import { InfoDialogComponent } from '../info-dialog/info-dialog.component';
 
 @Component({
   selector: 'app-login',
@@ -10,21 +12,22 @@ import {UserService} from '../../core/services/user.service';
   providers: [LoginService],
 })
 export class loginComponent implements OnInit {
-  email: string = '';
-  password: string = '';
+  email: string;
+  password: string;
+  loading: boolean;
 
-  abhishek = '';
-  name = '';
   constructor(
     public loginService: LoginService,
     private router: Router,
-    private userService: UserService
-  ) {}
+    private userService: UserService,
+    public dialog: MatDialog,
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   login() {
-    if (this.email && this.password) {
+    let validationResult = this.validate()
+    if (validationResult.status === true) {
       this.loginService.doLogIn(this.email, this.password).subscribe(
         (response: any) => {
           response['password'] = this.password; // added for use when fetching from localStorage, as there password doesn't get returned from the response
@@ -32,13 +35,28 @@ export class loginComponent implements OnInit {
           this.router.navigate(['/home']);
         },
         error => {
-          console.error(
-            `There is some issue with the login service ${JSON.stringify(
-              error
-            )}. Please contact administrator.`
-          );
+          console.log(error);
+          this.showDialog("Login Error:", error.error.message)
+          this.loading = false
         }
       );
+    } else {
+      this.showDialog("Validation Error", validationResult.message)
+      this.loading = false
     }
+  }
+
+  validate(): any {
+    if (!this.email) return { status: false, message: "Email cannot be empty" }
+    if (!this.password) return { status: false, message: "Password cannot be empty" }
+    return { status: true }
+  }
+
+  showDialog(title: string, message: string) {
+    let dialogRef = this.dialog.open(InfoDialogComponent, {
+      data: { title: title, message: message }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+    })
   }
 }
