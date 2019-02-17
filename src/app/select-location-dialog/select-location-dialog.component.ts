1533b16dcd292c8cb8a05c4d8d71fe6cb869360b
Abhishek Meher <devaskbiz@gmail.com>
2019-02-17 21:11:16 +0530

    Enhancement: Select location dialog now has option for current location.

--- src/app/select-location-dialog/select-location-dialog.component.ts
+++ src/app/select-location-dialog/select-location-dialog.component.ts
@@ -11,22 +11,37 @@ import { GeocodeService } from 'src/core/services/geocode.service';
 export class SelectLocationDialogComponent implements OnInit {
 
   address: ResolvedAddress
+  placesText: string
+  loading:boolean = false
   constructor(
-    private geocodeService: GeocodeService    
-  ) { }
+    private geocodeService: GeocodeService
+  ) {
+
+  }
 
   ngOnInit() {
   }
 
-  addressSelected(address){
-      this.address = {
-        latlng: {
-          latitude: address.geometry.location.lat(),
-          longitude: address.geometry.location.lng()
-        },
-        formattedAddress: address.formatted_address,
-        completeAddress: this.geocodeService.parseCompleteAddress(address.address_components)
-      }
+  addressSelected(address) {
+    this.address = {
+      latlng: {
+        latitude: address.geometry.location.lat(),
+        longitude: address.geometry.location.lng()
+      },
+      formattedAddress: address.formatted_address,
+      completeAddress: this.geocodeService.parseCompleteAddress(address.address_components)
+    }
+  }
+
+  currentLocationClicked() {
+    this.loading = true
+    this.geocodeService.getLatitudeLongitude(true).then(latlng => {
+      this.geocodeService.resolveAddress(latlng).subscribe(address => {
+        this.placesText = address.formattedAddress
+        this.address = address
+        this.loading = false
+      }, error=>{this.loading = false})
+    }).catch(err=>{this.loading = false})
   }
 
 }
